d985d619873df10e14bfbaecc909bed1eed94207
Madhurima Rawat <105432776+madhurimarawat@users.noreply.github.com>
2024-07-20 22:43:33 +0530

    Delete 5 basic program directory

--- 5 basic program/first-program-print-two-numbers-code.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
- int a,b;
-  cout<<"Enter two numbers:\n";
-cin>>a>>b;
-cout<<"Entered numbers are:"<<a<<"\n"<<""<<b;
-return 0;
-}
--- 5 basic program/firstmanipulation-without-namespace-code.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-int main(){
- int a,b;
-  std::cout<<"Enter two numbers:\n";
-std::cin>>a>>b;
-std::cout<<"Entered numbers are:"<<a <<" \n"<<b;
-return 0;
-}
